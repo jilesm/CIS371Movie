b0115aed79ce8dd4f2e733a2496d5aa51d501ade
khacker23 <khacker23@gmail.com>
2018-04-09 15:51:44 -0400

    add google chart to home.php

--- home.php
+++ home.php
@@ -7,10 +7,44 @@ require("auth.php");
 
 <html>
 <head>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+	    
+    function drawMaterial() {
+      var data = google.visualization.arrayToDataTable([
+        ['City', '2010 Population'],
+        ['New York City, NY', 8],
+        ['Los Angeles, CA', 37],
+        ['Chicago, IL', 26],
+        ['Houston, TX', 20],
+        ['Philadelphia, PA', 15]
+      ]);
+
+      var materialOptions = {
+        chart: {
+          title: 'Population of Largest U.S. Cities'
+        },
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'City'
+        },
+        bars: 'horizontal'
+      };
+      var materialChart = new google.charts.Bar(document.getElementById('chart_div'));
+      materialChart.draw(data, materialOptions);
+    }
+	</script>
+    
 	<title>Movie Ratings</title>
 </head>
 
 <body>
+	<div id="barchart_material" style="width: 900px; height: 500px;"></div>
 <?php 
 $l=mysqli_connect("34.224.83.184", "student4", "phppass4", "student4");
 

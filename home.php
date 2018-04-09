afa96bc3530ba40be63794482225655bb8a202c8
Marcus Jiles <jilesm@mail.gvsu.edu>
2018-04-09 14:23:03 -0400

    Add files via upload
    Updated and added a table to display the movie name.

--- home.php
+++ home.php
@@ -7,16 +7,34 @@ require("auth.php");
 
 <html>
 <head>
-	<title>Movie Rates</title>
+	<title>Movie Ratings</title>
 </head>
 
 <body>
 <?php 
-require("menu.php");
-echo "Welcome ".$_SESSION['auth'];
+$l=mysqli_connect("34.224.83.184", "student4", "phppass4", "student4");
+
+if(mysqli_connect_errno()){
+    echo "Connect could not be made" .mysqli_error();
+}
+
+$result = mysqli_query($l,"select * from movies2");
+
+echo "<table border='1'>
+<tr>
+<th>Movie Name</th>
+
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+    echo "<tr>";
+    echo "<td>" . $row['movieName'] . "</td>";
+}
+echo "</table>";
+
+mysqli_close($l);
 ?>
-
-
 </body>
 
 </html>

d3ece6eb8e41f7c3c2e2b0e2f1df76aaf7717ede
khacker23 <khacker23@gmail.com>
2018-04-10 14:55:59 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,17 +1,56 @@
-<?php 
+<?php
 session_start();
 
-require("auth.php");
+//require("auth.php");
 
 ?>
 
 <html>
 <head>
-	<title>Movie Ratings</title>
+<!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+        <title>Movie Ratings</title>
 </head>
 
 <body>
-<?php 
+        <div id="chart_div" style="width: 900px; height: 500px;"></div>
+<?php
+require("menu.php");
 $l=mysqli_connect("34.224.83.184", "student4", "phppass4", "student4");
 
 if(mysqli_connect_errno()){

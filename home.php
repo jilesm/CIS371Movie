5968d7555a50565b2d8de791801225fbecc2fbd9
Alex Glenn <glennal@mail.gvsu.edu>
2018-04-12 17:42:59 -0400

    change to home.php
    Hopefully home.php should now work

--- home.php
+++ home.php
@@ -1,63 +1,17 @@
 <?php 
 session_start();
-
-<<<<<<< HEAD
 //require("auth.php");
 $l=mysqli_connect("34.224.83.184", "student4", "phppass4", "student4");
 
 if(mysqli_connect_errno()){
     echo "Connect could not be made" .mysqli_error();
 }
-=======
-require("auth.php");
->>>>>>> parent of d3ece6e... Update home.php
 
 $result = mysqli_query($l,"select * from movies2");
 ?>
 
 <html>
-<head>
-<<<<<<< HEAD
-
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawChart);
-	    
-    function drawMaterial() {
-      var data = google.visualization.arrayToDataTable([
-        ['City', '2010 Population'],
-        ['New York City, NY', 8],
-        ['Los Angeles, CA', 37],
-        ['Chicago, IL', 26],
-        ['Houston, TX', 20],
-        ['Philadelphia, PA', 15]
-      ]);
-
-      var materialOptions = {
-        chart: {
-          title: 'Population of Largest U.S. Cities'
-        },
-        hAxis: {
-          title: 'Total Population',
-          minValue: 0,
-        },
-        vAxis: {
-          title: 'City'
-        },
-        bars: 'horizontal'
-      };
-      var materialChart = new google.charts.Bar(document.getElementById('chart_div'));
-      materialChart.draw(data, materialOptions);
-    }
-	</script>
-    
-	<title>Movie Ratings</title>
-</head>
-
-<body>
-	<div id="barchart_material" style="width: 900px; height: 500px;"></div>
-<?php 
+<head> 
 
 <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -97,9 +51,6 @@ $result = mysqli_query($l,"select * from movies2");
       }
     </script>
         <title>Movie Ratings</title>
-=======
-	<title>Movie Ratings</title>
->>>>>>> parent of d3ece6e... Update home.php
 </head>
 
 <header>
@@ -107,23 +58,8 @@ $result = mysqli_query($l,"select * from movies2");
 </header>
 
 <body>
-<<<<<<< HEAD
-        <div id="chart_div" style="width: 900px; height: 500px;"></div>
-<?php
-
-require("menu.php");
-
-=======
+        <div id="chart_div" style="width: 900px; height: 325px;"></div>
 <?php 
->>>>>>> parent of d3ece6e... Update home.php
-$l=mysqli_connect("34.224.83.184", "student4", "phppass4", "student4");
-
-if(mysqli_connect_errno()){
-    echo "Connect could not be made" .mysqli_error();
-}
-
-$result = mysqli_query($l,"select * from movies2");
-
 
 echo "<table border='1' cellpadding=10>";
 echo "<tr><th>Movie Name</th></tr>";

99d18af69c9cee852237aaf307a14222a2ccd60b
Marcus Jiles <jilesm@mail.gvsu.edu>
2018-04-15 16:13:05 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -5,8 +5,9 @@
 <body>
 <ul>
 <li> <a href="home.php">Home</a></li>
- <li><a href="donate.php">Donate</a></li>
+<li><a href="donate.php">Donate</a></li>
 <li><a href="help.php">Help</a></li>
+<li><a href="movie.php">Movies</a></li>
 <li><a href="logout.php">Logout</a></li>
 </ul>
 </body>

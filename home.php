57aef869e8127e6394ed9ef974def17dccbda138
Alex Glenn <glennal@mail.gvsu.edu>
2018-04-15 16:48:31 -0400

    Fix chart
    changes to home.php should fix chart

--- home.php
+++ home.php
@@ -1,20 +1,81 @@
-<?php 
+<?php
 session_start();
-require("auth.php");
+//require("auth.php");
 $l=mysqli_connect("34.224.83.184", "student4", "phppass4", "student4");
 
 if(mysqli_connect_errno()){
     echo "Connect could not be made" .mysqli_error();
 }
-
-$result = mysqli_query($l,"select * from movies2");
+if($bAndtheB = mysqli_query($l,"select count from movies2 where movie_id='beauty'")){
+                while($objBandB=mysqli_fetch_object($bAndtheB))
+                {
+                //echo "$objBandB->count";
+                $outputBandB= $objBandB->count;
+		}}
+if($despic = mysqli_query($l,"select count from movies2 where movie_id='despicable'")){
+                while($objDespic=mysqli_fetch_object($despic))
+                {
+                //echo "$objDespic->count";
+                $outputDespic= $objDespic->count;
+                }}
+if($fate= mysqli_query($l,"select count from movies2 where movie_id='fate'")){
+                while($objFate=mysqli_fetch_object($fate))
+                {
+                //echo "$objFate->count";
+                $outputFate= $objFate->count;
+                }}
+if($guardians= mysqli_query($l,"select count from movies2 where movie_id='guardians'")){
+                while($objGuardians=mysqli_fetch_object($guardians))
+                {
+                //echo "$objGuardians->count";
+                $outputGuardians= $objGuardians->count;
+                }}
+if($it= mysqli_query($l,"select count from movies2 where movie_id='it'")){
+                while($objIt=mysqli_fetch_object($it))
+                {
+                //echo "$objIt->count";
+                $outputIt= $objIt->count;
+                }}
+if($Logan= mysqli_query($l,"select count from movies2 where movie_id='logan'")){
+                while($objLogan=mysqli_fetch_object($Logan))
+                {
+                //echo "$objLogan->count";
+                $outputLogan= $objLogan->count;
+                }}
+if($spider= mysqli_query($l,"select count from movies2 where movie_id='spider'")){
+                while($objSpider=mysqli_fetch_object($spider))
+                {
+                //echo "$objSpider->count";
+                $outputSpider= $objSpider->count;
+                }}
+if($spider= mysqli_query($l,"select count from movies2 where movie_id='spider'")){
+                while($objSpider=mysqli_fetch_object($spider))
+                {
+                //echo "$objSpider->count";
+                $outputSpider= $objSpider->count;
+                }}
+if($star= mysqli_query($l,"select count from movies2 where movie_id='star'")){
+                while($objStar=mysqli_fetch_object($star))
+                {
+                //echo "$objSpider->count";
+                $outputStar= $objStar->count;
+                }}
+if($Thor= mysqli_query($l,"select count from movies2 where movie_id='Thor'")){
+                while($objThor=mysqli_fetch_object($Thor))
+                {
+                //echo "$objThor->count";
+                $outputThor= $objThor->count;
+                }}
+if($wonder= mysqli_query($l,"select count from movies2 where movie_id='wonder'")){
+                while($objWonder=mysqli_fetch_object($wonder))
+                {
+                //echo "$objWonder->count";
+                $outputWonder= $objWonder->count;
+                }}
 ?>
-
 <html>
 <head> 
-<link rel="stylesheet" type="text/css" href="home.css">
 
-    
 <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
@@ -32,17 +93,21 @@ $result = mysqli_query($l,"select * from movies2");
 
         // Create the data table.
         var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
+        data.addColumn('string', 'Movie');
+        data.addColumn('number', 'Number of Students');
         data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        // Set chart options
+          ['Beauty and the Beast',<?php echo $outputBandB;?>],
+	  ['Despicable Me 3', <?php echo $outputDespic;?>],
+	  ['Fate of the Furious', <?php echo $outputFate; ?>],
+          ['Guardians of the Galaxy: Vol 2.', <?php echo $outputGuardians; ?>],
+          ['IT', <?php echo $outputIt; ?>],
+	  ['Logan', <?php echo $outputLogan; ?>],
+          ['Spider-Man: Homecoming', <?php echo $outputSpider; ?>],
+          ['Star Wars: The Force Awakens', <?php echo $outputStar; ?>],
+          ['Thor: Ragnarok', <?php echo $outputThor; ?>],
+          ['Wonder Woman', <?php echo $outputWonder; ?>]
+	  ]);
+ // Set chart options
         var options = {'title':'How Much Pizza I Ate Last Night',
                        'width':400,
                        'height':300};
@@ -54,7 +119,6 @@ $result = mysqli_query($l,"select * from movies2");
     </script>
         <title>Movie Ratings</title>
 </head>
-
 <header>
 <?PHP require("menu.php");?>
 </header>
@@ -63,7 +127,7 @@ $result = mysqli_query($l,"select * from movies2");
         <div id="chart_div" style="width: 900px; height: 325px;"></div>
 <?php 
 
-echo "<table border='1' cellpadding=10 id='display'>";
+echo "<table  border='1' cellpadding=10>";
 echo "<tr><th>Movie Name</th></tr>";
 
 while($row = mysqli_fetch_array($result))
@@ -77,7 +141,7 @@ while($row = mysqli_fetch_array($result))
 }
 echo "</table>";
 
-mysqli_close($l);
+
 ?>
 </body>
 

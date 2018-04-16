f20e1d96701c1ba812f5f63f695656ce8b047204
Alex Glenn <glennal@mail.gvsu.edu>
2018-04-16 15:10:08 -0400

    Fix errors in help.php

--- help.php
+++ help.php
@@ -1,18 +1,12 @@
 
 <?php
 require("menu.php");
-echo "<br /><br />Our home page allows you to select which movies were your favorite <br /><$
+echo "<br /><br />Our home page allows you to select which movies were your favorite <br /><br />";
 
 echo "Please assist us on growing this website by clicking our donate button<br /><br />";
 
 echo "Help well you are already here enjoy this picture<br /><br />";
 
-echo "Click this to logout. Do it leave us.<br /><br />"
-
+echo "Click this to logout. Do it leave us.<br /><br />";
 
 ?>
-
-
-
-
-
